89dd3d4266411a75159c7ac6f12e500a82c3f43c
vaishnavh <vaishnavh.nagarajan@gmail.com>
2015-11-28 23:29:00 -0500

    Fixed video script

--- sbin/video.sh
+++ sbin/video.sh
@@ -2,21 +2,21 @@
 chmod u+x ./* 
 
 echo "[DEMO] Start Basic SGD..."
-output=$1+"_basic_sgd"
+output=$1"_basic_sgd"
 mkdir $output
-./run_basic_sgd.sh ../data/video/corrupt_10.csv $output $2 4 $3 $4 $5 240 320 3 141 ../data/video/test.csv
+./run_basic_sgd.sh ../data/video/corrupt_10.csv $output $2 4 $3 $4 $5 240 320 3 141 ../data/video/true_10.csv ../data/video/true_10.csv
 echo "[DEMO] Finish Basic SGD..."
 echo "[DEMO] Outputs are saved in a local directory"
 
 echo "[DEMO] Start PTF SGD..."
-output=$1+"_ptf_sgd"
+output=$1"_ptf_sgd"
 mkdir $output
 ./run_kptf_sgd.sh ../data/video/corrupt_10.csv $output $2 4 $3 $4 $5 240 320 3 141 None:1 None:1 None:1 None:1 ../data/video/test.csv
 echo "[DEMO] Finish PTF SGD..."
 echo "[DEMO] Outputs are saved in a local directory"
  
 echo "[DEMO] Start KPTF SGD..."
-output=$1+"_kptf_sgd"
+output=$1"_kptf_sgd"
 mkdir $output
 ./run_kptf_sgd.sh ../data/video/corrupt_10.csv $output $2 4 $3 $4 $5 240 320 3 141 RBF:$6 RBF:$6 None:1 RBF:$7 ../data/video/test.csv
 echo "[DEMO] Finish PTF SGD...e"
--- sbin/video_kptf.sh
+++ sbin/video_kptf.sh
@@ -1,6 +1,6 @@
 
 chmod u+x ./* 
 echo "[DEMO] Start KPTF SGD..."
-output=$1+"_kptf_sgd"
+output=$1"_kptf_sgd"
 mkdir $output
 ./run_kptf_sgd.sh ../data/video/corrupt_10.csv $output $2 4 $3 $4 $5 240 320 3 141 RBF:$6 RBF:$6 None:1 RBF:$7 ../data/video/test.csv
