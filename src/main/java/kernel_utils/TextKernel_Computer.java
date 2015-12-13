388af8ed64e7dc8d6ce8cd8b310ab149e9b63b72
kijungshin <kijung.shin89@gmail.com>
2015-12-13 14:55:31 -0500

    Fix compile error

--- src/main/java/kernel_utils/TextKernel_Computer.java
+++ src/main/java/kernel_utils/TextKernel_Computer.java
@@ -106,7 +106,7 @@ public class TextKernel_Computer {
 		double w2 = 1.0 - w1;
 		Set<K> allKeys = getSetOfAllKeys(d1, d2);
 		int numKeys = d1.getNumberOfKeys();
-		Counter<K> c = new ClassicCounter<>();
+		Counter<K> c = new ClassicCounter();
 
 		for (K key : allKeys){
 			double newProbability = d1.probabilityOf(key) * w1 + d2.probabilityOf(key) * w2;
